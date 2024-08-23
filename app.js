a8a8d95f48eb6173b3e3859b972ab327d8e16e39
Aiushi Sengupta <aiushisengupta26@gmail.com>
2024-08-24 02:25:38 +0530

    Changed port

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ mongoose
 		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fl9kegz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
 	)
 	.then(() => {
-		app.listen(5000);
+		app.listen(443);
 	})
 	.catch((err) => {
 		console.log(err);
